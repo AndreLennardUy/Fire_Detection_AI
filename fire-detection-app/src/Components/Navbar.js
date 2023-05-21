12380bc4e4ff13f8211c7befe4931631f317fc30
John William Miones <johnwilliam.miones@cit.edu>
2023-05-21 13:40:34 +0800

    Add design

--- fire-detection-app/src/Components/Navbar.js
+++ fire-detection-app/src/Components/Navbar.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 import logo from './images/flames.png';
 import './css/navbar.css';
 
-export const Navbar = () => {
+export const Navbar = ({ activeRoute }) => {
   return (
     <>
       <nav className="navbar">
         <img src={logo} alt="logo" className="navbar-logo" />
-        <Link to="/" className="navbar-link-about">About</Link>
-        <Link to="/Fire-Detection" className="navbar-link-fire">Fire Detection</Link>
+        <Link to="/" className={`navbar-link ${activeRoute === '/' ? 'active' : ''}`}>
+          About
+        </Link>
+        <Link
+          to="/fire-detection"
+          className={`navbar-link ${activeRoute === '/fire-detection' ? 'active' : ''}`}
+        >
+          Fire Detection
+        </Link>
       </nav>
     </>
   );
